45ec7e4f3f27ce4e0bf8b41508f4987d2234e8a8
Hernan <hernan@jsatch.com>
2021-06-10 19:48:57 -0500

    Utilizando sesiones

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const session = require('express-session')
 const getVideojuegos = require('./models/dao_videojuegos')
-
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
 app.use(express.static('assets')) // configurar archivos estaticos
 app.set('view engine', 'ejs') // configurar ejs template
+app.use(bodyParser.json()) // para trabajar con formularios
+app.use(bodyParser.urlencoded({
+    extended : true
+})) // para trabajar con formularios
+app.use(session({
+    secret : "123456789",
+    resave : false,
+    saveUninitialized : false
+})) // configurando el manejo de sesiones
 
 app.get('/', (req, res)=> {
-    res.render('index')
+    // Valido si hay un usuario en la sesion
+    if (req.session.usuario != null) {
+        res.render('index', {
+            usuario : req.session.usuario
+        })
+    }else {
+        res.render('index', {
+            usuario : {
+                nombre : "",
+                mail : "",
+                mensaje : ""
+            }
+        })
+    }
+    
 })
 
 app.get('/catalogo', (req, res) => {
@@ -16,6 +40,21 @@ app.get('/catalogo', (req, res) => {
     res.render('catalogo', {
         videojuegos : listaVideojuegos
     })
+})
+
+app.post('/registro', (req, res) => {
+    const usuario = {
+        nombre : req.body.nombre,
+        pais : req.body.pais,
+        mail : req.body.mail,
+        mensaje : req.body.mensaje
+    }
+
+    // Guardarlo en sesion
+    req.session.usuario = usuario
+
+    res.redirect('/')
+
 })
 
 app.listen(PORT, ()=> {
